6d874f1a302cb22a28fd96be401c837184c5cb0d
James Harvey <jharvey407@users.noreply.github.com>
2020-06-26 06:43:53 -0400

    Added Output for Testing, Logic is bad

--- Springboard/Insertion Sort.py
+++ Springboard/Insertion Sort.py
@@ -7,16 +7,21 @@ Created on Fri Jun 26 06:15:17 2020
 
 
 def insertionSort(lst):
+    print(lst)
     # Main loop
     # Loop through all values in list starting with the second
     for i in range(1, len(lst)):
         # Second loop
         # Loop through all values in list
-        for j in range(len(lst)):
+        
+        for j in range(0,i):
+            print('Test ', lst[i], '<', lst[j])
             # If current value is smaller then the test value swap values
             if lst[i] < lst[j]:
                 lst[i], lst[j] = lst[j], lst[i]
-                
+                print(lst)
+            
+        print(lst)
     return lst
 
 test0 = [3, 22, 14, 434, 501, 11, 9, 1230, 304, 123, 5412, 381923, 302, -3, 1]
@@ -28,9 +33,10 @@ test2 = [100, 0, 0, -20, 30, -5]
 test3  = [28, 4, 17, 666, 1001, 52, 61, 30]
 
 print(insertionSort(test0))
-print()
+'''print()
 print(insertionSort(test1))
 print()
 print(insertionSort(test2))
 print()
 print(insertionSort(test3))
+'''
