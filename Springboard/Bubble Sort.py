2d251637bc25509baee56cf5429244aa31f7d61c
James Harvey <jharvey407@users.noreply.github.com>
2020-06-26 05:48:19 -0400

    Added output for illustration

--- Springboard/Bubble Sort.py
+++ Springboard/Bubble Sort.py
@@ -8,10 +8,18 @@ Created on Thu Jun 25 06:49:03 2020
 # Second test commit, without pushing the first commit
 
 def bubbleSort(lst):
+    # Primary loop
+    # Loop through the list the number of items in the list
     for j in range(len(lst)):
+        # Secondary loop
+        # Loop throuhg the list swapping hihger numbers with lower numbers
         for i in range(1,len(lst)):
+            # Test if this number is smaller then the previous number
             if lst[i] < lst[i-1]:
+                # Swap the numbers
                 lst[i], lst[i-1] = lst[i-1], lst[i]
+        # Output for illustration purposes
+        print(lst)    
     return(lst)
 
 test0 = [3, 22, 14, 434, 501, 11, 9, 1230, 304, 123, 5412, 381923, 302, -3, 1]
@@ -23,6 +31,9 @@ test2 = [100, 0, 0, -20, 30, -5]
 test3  = [28, 4, 17, 666, 1001, 52, 61, 30]
 
 print(bubbleSort(test0))
+print()
 print(bubbleSort(test1))
+print()
 print(bubbleSort(test2))
+print()
 print(bubbleSort(test3))
