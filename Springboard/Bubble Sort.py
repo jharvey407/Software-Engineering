7cd9687d3b68eb7cfd5ec784e10ca30746729c0e
James Harvey <jharvey407@users.noreply.github.com>
2020-06-26 06:14:03 -0400

    Reconfigured Loops, Improved Output

--- Springboard/Bubble Sort.py
+++ Springboard/Bubble Sort.py
@@ -8,18 +8,21 @@ Created on Thu Jun 25 06:49:03 2020
 # Second test commit, without pushing the first commit
 
 def bubbleSort(lst):
+    print(lst)
     # Primary loop
     # Loop through the list the number of items in the list
     for j in range(len(lst)):
         # Secondary loop
-        # Loop throuhg the list swapping hihger numbers with lower numbers
-        for i in range(1,len(lst)):
+        # Loop throuhg the list swapping higher numbers with lower numbers
+        output = []
+        for i in range(1,len(lst) - j):
             # Test if this number is smaller then the previous number
             if lst[i] < lst[i-1]:
                 # Swap the numbers
                 lst[i], lst[i-1] = lst[i-1], lst[i]
-        # Output for illustration purposes
-        print(lst)    
+            output.append(lst[i-1])
+        print(output)
+            
     return(lst)
 
 test0 = [3, 22, 14, 434, 501, 11, 9, 1230, 304, 123, 5412, 381923, 302, -3, 1]
